10e33e497276055d4e4aa2a3ac8f4ef3f6b46009
Brian Diaz <bidiaz101@gmail.com>
2021-12-27 18:30:08 -0600

    completed assignment

--- ruby/my_set.rb
+++ ruby/my_set.rb
@@ -4,34 +4,82 @@ class MySet
   # if an iterable is provided only its unique values should be in data
   # strings and arrays will need to be broken down by their elements/characters
   def initialize(iterable = nil)
+    raise 'must be called with nothing, a string, or an array' unless
+      iterable.nil? || iterable.kind_of?(Array) || iterable.kind_of?(String)
+
     @data = {}
+
+    unless iterable.nil? 
+      items = iterable.kind_of?(String) ? iterable.split('') : iterable
+
+      items.each { |el| data[el] = true }
+    end
   end
 
   # return number of elements in MySet
   def size
+    @data.length
   end
 
   # add an item to MySet as is
   # return the MySet instance
   def add(item)
+    @data.store(item, true)
+    self
   end
 
   # delete an item from MySet
   # return true if successful otherwise false
   def delete(item)
+    new_data = @data.select! { |key| key != item }
+    !!new_data
   end
 
   # return true if in MySet, otherwise false
   def has(item)
+    @data.member?(item)
   end
 
   # return data as an array
   def entries
+    @data.keys
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  # Don't forget to add your own!
+  mySet = MySet.new
+  puts "'empty', #{mySet.data}"
+  puts "'size', #{mySet.size}"
+  puts "'entries', #{mySet.entries}"
+
+  mySet = MySet.new([1, 2, 1, 3])
+  puts "'with array [1, 2, 1, 3]', #{mySet.data}"
+  puts "'size', #{mySet.size}"
+  puts "'entries', #{mySet.entries}"
+
+  mySet = MySet.new('hello')
+  puts "'with string hello', #{mySet.data}"
+  puts "'size', #{mySet.size}"
+  puts "'entries',#{mySet.entries}"
+
+  puts ''
+  puts 'ADD STUFF'
+  puts mySet.add('adding')
+  print mySet.data
+  puts
+  puts mySet.add(5)
+  print mySet.data
+  puts
+
+  puts ''
+  puts 'HAS STUFF'
+  puts mySet.has('adding')
+  puts mySet.has(10000)
+
+  puts ''
+  puts 'DELETE STUFF'
+  puts mySet.delete('adding')
+  puts mySet.delete(10000)
 end
 
 
